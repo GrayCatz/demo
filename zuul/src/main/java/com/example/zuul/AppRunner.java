0dc444d8a758bb9a5dce7154df72375dabbadc02
zain <1103038194@qq.com>
2019-07-15 03:00:20 +0800

    修改zuul

--- zuul/src/main/java/com/example/zuul/AppRunner.java
+++ zuul/src/main/java/com/example/zuul/AppRunner.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class AppRunner {
     public static void main(String[] args) {
