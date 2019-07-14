867abf59c64fb7443e096f261f77973aaad19aac
zain <1103038194@qq.com>
2019-07-15 00:36:26 +0800

    添加 log4j2支持

--- service/user-service/src/main/java/com/example/user/rest/UserController.java
+++ service/user-service/src/main/java/com/example/user/rest/UserController.java
@@ -1,6 +1,8 @@
 package com.example.user.rest;
 
 import com.example.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/testBoot")
 public class UserController {
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable int id) {
+        logger.debug("debut_test:");
+        logger.info("debut_test:");
         return userService.Sel(id).toString();
     }
 }
