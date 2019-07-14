43523b2cd890dd41c9ecbc2cf6498296b17a5d95
zain <1103038194@qq.com>
2019-07-15 02:24:30 +0800

    添加 log4j2支持

--- service/user-service/src/main/java/com/example/user/AppRunner.java
+++ service/user-service/src/main/java/com/example/user/AppRunner.java
@@ -15,6 +15,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @MapperScan("com.example.user.mapper") //扫描的mapper
 public class AppRunner {
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "dev");// 指定开发环境配置
+        System.setProperty("spring.application.name", "user-service");// 应用日志名称
         SpringApplication.run(AppRunner.class, args);
     }
 }
