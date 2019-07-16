399c5c0c299eebc332ff9facad52c039db75d277
zain <1103038194@qq.com>
2019-07-16 13:19:55 +0800

    加入mybatis自动建表

--- service/user-service/src/main/java/com/example/user/entity/User.java
+++ service/user-service/src/main/java/com/example/user/entity/User.java
@@ -1,9 +1,18 @@
 package com.example.user.entity;
 
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+
+@Table(name = "user")
 public class User {
+    @Column(name = "id", type = MySqlTypeConstant.INT, length = 11, isKey = true, isAutoIncrement = true)
     private Integer id;
+    @Column(name = "user_name", type = MySqlTypeConstant.VARCHAR, length = 256)
     private String userName;
+    @Column(name = "pass_word", type = MySqlTypeConstant.VARCHAR, length = 256)
     private String passWord;
+    @Column(name = "real_name", type = MySqlTypeConstant.VARCHAR, length = 256)
     private String realName;
 
     public Integer getId() {
